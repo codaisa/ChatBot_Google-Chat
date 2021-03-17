4d8de62ae0c268c34ff52387ebfeb4268d86c7fa
Elzevir De Sousa Sá Filho <safilhoelzevirsafilho@gmail.com>
2021-03-17 20:00:35 -0300

    teste1

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require('express')
 const app = express()
-const port = 587
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:`)
-})
+app.listen('/')
