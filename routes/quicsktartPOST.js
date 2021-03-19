a0f944b3e48b742fdbc9b986bd5692f6bf82b80d
Elzevir De Sousa Sá Filho <safilhoelzevirsafilho@gmail.com>
2021-03-19 16:53:15 -0300

    ajusteAPI

--- routes/quicsktartPOST.js
+++ routes/quicsktartPOST.js
@@ -1,14 +1,13 @@
 const {google} = require('googleapis');
 
-exports.sendMessage = async (messageValue, spaceValue)=>{
+exports.sendMessage = async (card, spaceValue)=>{
   const chat = google.chat({
     version: 'v1',
     auth: 'AIzaSyDm-K5Nh9dDqY_7moHovfURZVvpMhJOSHI' // specify your API key here
   });
   
   var scopes = 'https://www.googleapis.com/auth/chat.bot'
-  var content
-
+  
   async function main() {
     const auth = new google.auth.GoogleAuth({
       // Scopes can be specified either as an array or as a single, space-delimited string.
@@ -16,11 +15,13 @@ exports.sendMessage = async (messageValue, spaceValue)=>{
     });
     
     // Do the magic
+
     const res = await chat.spaces.messages.create({
       parent: 'spaces/' + spaceValue,
       // Opaque thread identifier string that can be specified to group messages into a single thread. If this is the first message with a given thread identifier, a new thread is created. Subsequent messages with the same thread identifier will be posted into the same thread. This relieves bots and webhooks from having to store the Hangouts Chat thread ID of a thread (created earlier by them) to post further updates to it. Has no effect if thread field, corresponding to an existing thread, is set in message.
       
       // Request body metadata
+
       requestBody: {
         // request body parameters
         // {
@@ -28,14 +29,7 @@ exports.sendMessage = async (messageValue, spaceValue)=>{
           "annotations": [],
           "argumentText": "my_argumentText",
           "attachment": [],
-          "cards": [
-            createHeaderWithURL("Pessoal, vocês já apontaram as horas hoje? ","Talvez esteja na hora de apontar.",doubtsIcon),
-            {
-              "sections": [{
-                "widgets": apontamentoHoras
-              }]
-            }
-          ],
+          "cards": [card],
           "fallbackText": "my_fallbackText",
           "name": "my_name",
           "sender": {},
@@ -53,43 +47,5 @@ exports.sendMessage = async (messageValue, spaceValue)=>{
       console.error(e);
       throw e;
     });
-
-/*     RESPONSE CARD         */
-var doubtsIcon = "https://media.giphy.com/media/PidtQ2uWrHqxhBnhOa/giphy.gif";
-
-  function createHeaderWithURL(title, subtitle,url){
-    return {
-    "header": {
-      "title" : title,
-      "subtitle" : subtitle,
-      "imageUrl" : url
-    }}; 
-    }
 }
-
-var apontamentoHoras = [{
-  "textParagraph": {
-    "text": "<b>Basta escolher qual hora você deseja apontar."
-  }
-},{
-  "buttons": [{
-    "textButton": {
-      "text": "APONTAR HORA DE ENTRADA",
-      "onClick": {
-        "action": {
-          "actionMethodName": "HORAENTRADA"
-        }
-      }
-    }
-  }, {
-    "textButton": {
-      "text": "APONTAR HORA DE SAÍDA",
-      "onClick": {
-        "action": {
-          "actionMethodName": "HORASAIDA"
-        }
-      }
-    }
-  }]
-}];
   
--- routes/sendMessage.js
+++ routes/sendMessage.js
@@ -2,25 +2,39 @@ const express = require('express');
 const router = express.Router();
 
 const callFunction = require('./quickstart.js')
+const callFunctionPOST = require('./quicsktartPOST.js')
 
 
-router.get('/', (req, res, next) =>{
+router.get('/', (req, res, next) => {
     res.status(200).send({
-        mensagem:'Ok! Procedimento concluído.'
+        mensagem: 'Ok! Procedimento concluído.'
     });
 });
 
- router.get('/:space/:message', (req, res, next) =>{
+router.get('/:space/:message', (req, res, next) => {
     var space = req.params.space
     const msg = req.params.message
     space = space.split()[0]
     callFunction.sendMessage(msg, space)
 
-        res.status(200).send({
-            mensagem:'Ok, o processo foi chamado.',
-            space: space,
-            msg: msg,
-        });
-}); 
+    res.status(200).send({
+        mensagem: 'Ok, o processo foi chamado.',
+        space: space,
+        msg: msg,
+    });
+});
+
+
+router.use(express.json()) // for parsing application/json
+router.use(express.urlencoded({ extended: true }))  
+router.post('/:space', (req, res, next) => {
+    var resBody = req.body
+    var space = req.params.space
+    callFunctionPOST.sendMessage(resBody,space)
+    res.status(201).send({
+        status: 'Ok, o card foi emitido. ',
+        cardResponse:  resBody
+    });
+});
 
 module.exports = router;
